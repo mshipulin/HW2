618704a55644363f850954cc6045366242dfefb2
Max <m-shipulin@mail.ru>
2021-12-13 00:16:56 +0300

    HW2 first commit

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    
+    
+    @IBAction func redSliderAction() {
+        let red = round(redSlider.value * 100) / 100
+        let green = round(greenSlider.value * 100) / 100
+        let blue = round(blueSlider.value * 100) / 100
+        redLabel.text = String(red)
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(red),
+            green: CGFloat(green),
+            blue: CGFloat(blue), alpha: 1)
+    
+    }
+    
+    @IBAction func greenSliderAction() {
+        let red = round(redSlider.value * 100) / 100
+        let green = round(greenSlider.value * 100) / 100
+        let blue = round(blueSlider.value * 100) / 100
+        greenLabel.text = String(green)
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(red),
+            green: CGFloat(green),
+            blue: CGFloat(blue), alpha: 1)
+    }
+    @IBAction func blueSliderAction() {
+        let red = round(redSlider.value * 100) / 100
+        let green = round(greenSlider.value * 100) / 100
+        let blue = round(blueSlider.value * 100) / 100
+        blueLabel.text = String(blue)
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(red),
+            green: CGFloat(green),
+            blue: CGFloat(blue), alpha: 1)
+    }
 
 }
 
